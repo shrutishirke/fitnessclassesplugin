3a76e29043097e27d6bab56ff1ee48a0f47372a8
nayabs <nabi.safdar@gmail.com>
2016-03-28 23:07:47 -0400

    Delete monthlyspecials-shortcode.php
    unnecessary.

--- monthlyspecials-shortcode.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: Monthly Fitness Plugin
-Plugin URI: http://www.phoenix.sheridanc.on.ca/~ccit3424/
-Description: This widget displays the monthly specials divided by the week offered through PhireFitness. 
-Author: Nayab Safdar, Mayank Sharma, Shruti Shirke (MSN Developers)
-Version: 1.0 
-Author URI: http://phoenix.sheridanc.on.ca/~ccit3424
-*/
