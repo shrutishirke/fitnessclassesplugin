88fcf0f6af9a0a6c7976532d85d38e6e68c4fde7
nayabs <nabi.safdar@gmail.com>
2016-03-29 15:15:58 -0400

    Update monthlyspecials.php
    Changed the plugin name.

--- monthlyspecials.php
+++ monthlyspecials.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Monthly Fitness Widget
+Plugin Name: Monthly Specials 
 Plugin URI: http://www.phoenix.sheridanc.on.ca/~ccit3424/
 Description: This widget displays the monthly specials divided by the week offered through PhireFitness. 
 Author: Nayab Safdar, Mayank Sharma, Shruti Shirke (MSN Developers)
